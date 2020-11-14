5f8b592fa7e604203f9f3aa741a49edd07168805
Maximilian <max.helmersson@gmail.com>
2020-11-14 14:51:46 +0100

    Try to fix deploy issues 10

--- startup/routes.js
+++ startup/routes.js
@@ -17,6 +17,13 @@ const signup = require('../routes/api/signup');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
+  if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    });
+  }
+
   app.use(cors());
   app.use(morgan('tiny'));
   app.use(bodyParser.json());
